afc7bff4cd340c4c7e4208f413709e18e95ef284
Jianfeng Chen <352871242@qq.com>
2019-02-26 11:28:12 +0800

    Update test.php

--- API/test.php
+++ API/test.php
@@ -6,7 +6,7 @@
         $a->createSQLAndRun("select * from stu where id=%s and name='%s'",12312,"dkf--sdjf..sj");
         die("");
         //$a->loginFromXMU("23020161153315","1591147")
-        if ($a->login("23020161153315","159147")){
+        if ($a->login("23020161153315","111111")){
             echo "登陆成功";
         }else{
             echo $a->error_msg;
