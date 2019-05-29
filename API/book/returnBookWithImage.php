469d77df4a4d0c5d2e02ef3ce628507e61533632
vcvycy <352871242@qq.com>
2019-05-29 21:24:40 +0800

    还书拍照图片显示

--- API/book/returnBookWithImage.php
+++ API/book/returnBookWithImage.php
@@ -13,7 +13,8 @@ function main(){
         $book_borrow_id = $b->getBorrowIDNotReturn($sid,$isbn); 
         $save_path_without_suffix = sprintf("%suploads/return_book_images/return_book_image_%d",DIR_API,$book_borrow_id);//上传路径 
         $path = Utils::saveUploadedFile("image",$save_path_without_suffix,"jpg|jpeg|bmp|gif|png",5);  
-        $b->returnBook($sid,$isbn);
+        $filename=basename($path);
+        $b->returnBook($sid,$isbn,$filename);
         Utils::exit(0,"还书成功");
     } catch (Exception $e) {
         Utils::exit(-2,$e->getMessage());
